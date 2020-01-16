a1c8b9c450c515c7483d2a8b9520e10e03d3d635
CandyMan <c.mattson318@gmail.com>
2020-01-15 19:32:35 -0700

    created gif output to page ajax call. create gif onClick to switch between still and animate displays.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var gifSelection =["cats", "dogs", "memes", "random"];
+var gifSelection =["cats", "dogs", "memes", "random", "shoebill crane"];
 
 // on click function to add the input value inside the form and run the createButton function
 
@@ -24,10 +24,65 @@ $("#submit").on("click", function(){
     console.log(inputVal);
     createButton();
 });
+
 createButton();
 
 $(document).on("click", ".gifButton", function() {
     event.preventDefault();
-    console.log($(this).attr("value"));
+    var buttonSelect = $(this).attr("value")
+    console.log(buttonSelect);
+
+    var movie = buttonSelect;
+    var apiKey = "Ptje1aKgDLlEbocNFZqUE7a8u8zBo7wf";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonSelect + "&api_key=" + apiKey + "&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#imgDump").empty();
+      
+        for (var i = 0; i < response.data.length; i++) {
+            var div = $("<div>");
+            div.addClass("imgDump");
+
+            var img = $("<img>");
+            img.attr("src", response.data[i].images.fixed_height_still.url);
+            img.attr("data-still", response.data[i].images.fixed_height_still.url);
+            img.attr("data-animate", response.data[i].images.fixed_height.url)
+            img.attr("data-state", "still")
+            // img.attr("data-state", "animate");
+            img.addClass("gifs");
+
+            var rate = $("<p>");
+            rate.addClass("rated");
+            rate.text(response.data[i].rating);
+
+            var favButton = $("<button>");
+            favButton.addClass("favorite");
+            favButton.text("Favorite");
+
+            div.append(img);
+            div.append(rate);
+            div.append(favButton);
+
+            $("#imgDump").append(div);
+
+        };
+    });
+
 });
 
+$(document).on("click", ".gifs", function() {
+    event.preventDefault();
+    var state = $(this).attr("data-state");
+
+    if (state == "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
