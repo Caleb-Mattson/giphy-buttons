9bd69bd00b48a29e0f936e582188b3e56baeef7c
CandyMan <c.mattson318@gmail.com>
2020-01-18 00:15:33 -0700

    removed favorite button, not functioning as intended. Switched from onClick to hover function for gifs to change from static to animated. Created a clear button to empty imgDump div of gifs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
-var gifSelection =["cats", "dogs", "memes", "random", "shoebill crane"];
+var gifSelection =["community", "miracle workers", "third rock from the sun", "ap bio", "avatar the last airbender", "the witcher", "vikings"];
 
 // on click function to add the input value inside the form and run the createButton function
 
-
+// created function to populate page with buttons from array
 var createButton = function() {
     $("#button").empty();
 
+    // for looped through array to create buttons
     for (var i = 0; i < gifSelection.length; i++) {
         var a = $("<button>");
         a.addClass("gifButton");
@@ -16,7 +17,7 @@ var createButton = function() {
 };
 
 
-
+// create new button on click to push new value into array, running createButton function to populate new items in the array
 $("#submit").on("click", function(){
     event.preventDefault();
     var inputVal = $("#buttonCreate").val().trim();
@@ -24,6 +25,11 @@ $("#submit").on("click", function(){
     console.log(inputVal);
     createButton();
 });
+
+// empty out the imgDump div of gifs
+$("#clear").on("click", function(){
+    $("#imgDump").empty();
+})
 
 createButton();
 
@@ -35,6 +41,7 @@ $(document).on("click", ".gifButton", function() {
     var apiKey = "Ptje1aKgDLlEbocNFZqUE7a8u8zBo7wf";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonSelect + "&api_key=" + apiKey + "&limit=10";
 
+    // classic ajax call
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -43,10 +50,12 @@ $(document).on("click", ".gifButton", function() {
         $("#imgDump").empty();
       
         for (var i = 0; i < response.data.length; i++) {
+            // created individual divs for the gifs to sit in
             var div = $("<div>");
             div.addClass("imgDump");
             div.attr("id", "imgDiv-" + i);
 
+            // populated gifs images from API
             var img = $("<img>");
             img.attr("src", response.data[i].images.fixed_height_still.url);
             img.attr("data-still", response.data[i].images.fixed_height_still.url);
@@ -54,46 +63,53 @@ $(document).on("click", ".gifButton", function() {
             img.attr("data-state", "still")
             img.addClass("gifs");
 
+            // created rating tags for each image
             var rate = $("<p>");
             rate.addClass("rated");
-            rate.text(response.data[i].rating);
+            rate.text("Rated: " + response.data[i].rating);
 
-            var favButton = $("<button>");
-            favButton.attr("id", "favorite-" + i);
-            favButton.text("Favorite");
+            // Couldn't figure out favorite button
+            // var favButton = $("<button>");
+            // favButton.attr("id", "favorite-" + i);
+            // favButton.text("Favorite");
 
+            // appended images and rating to each div
             div.append(img);
             div.append(rate);
-            div.append(favButton);
+            // div.append(favButton);
 
+            // appended it all into the imgDump div anchor inside DOM
             $("#imgDump").append(div);
 
         };
 
-        // $(".favorite").on("click", function() {
-        //     var favorite = $(this).parent("id", "imgDiv-5");
-        //     $("#favImgDump").append(favorite);
-        //     console.log(favorite);
-        // });
+        // on hover function to show animated state
+        $(function() {
+            $(".gifs").hover(
+                function() {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                },
+                function() {
+                    $(this).attr("src", $(this).attr("data-still"));
+                }                         
+            );                  
+        });
 
     });
 
 });
 
-$(document).on("click", ".gifs", function() {
-    event.apventDefault();
-    var state = $(this).attr("data-state");
+// HERE IS WHERE I HAD MY ON CLICK TO CHANGE DATA STATES FROM STILL > ANIMATE
+// $(document).on("click", ".gifs", function() {
+//     event.preventDefault();
+//     var state = $(this).attr("data-state");
 
-    if (state == "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
-
-// $(document).on("click", ".favorite", function() {
-//     var favorite = $(this).parent("div");
-//     $(".favImgDump").append(div);
+//     if (state == "still") {
+//         $(this).attr("src", $(this).attr("data-animate"));
+//         $(this).attr("data-state", "animate");
+//     } else {
+//         $(this).attr("src", $(this).attr("data-still"));
+//         $(this).attr("data-state", "still");
+//     }
 // });
+
